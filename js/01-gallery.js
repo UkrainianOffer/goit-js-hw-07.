17897ce98133f0170743dba1dd0bcca72e0c8d6f
Ilya Chachua <cacuaila@gmail.com>
2022-01-09 13:58:51 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
+const galleryMarkUp = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+    <a
+      class="gallery__link"
+      href="${original}"      
+    >
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+   </a>
+   </div>
+`;
+  })
+  .join("");
 
-console.log(galleryItems);
+galleryEl.insertAdjacentHTML("afterbegin", galleryMarkUp);
+galleryEl.addEventListener("click", openModal);
+
+function openModal(e) {
+  e.preventDefault();
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" width="800px" height="600px">`
+  );
+  instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const galleryMarkUp = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a
+      class="gallery__link"
+      href="${original}"      
+    >
+      <img
+        class="gallery__image"
+        src="${preview}"       
+        alt="${description}"
+      />
+   </a>`;
+  })
+  .join("");
+
+galleryEl.insertAdjacentHTML("afterbegin", galleryMarkUp);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  /*options */ captions: "true",
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
